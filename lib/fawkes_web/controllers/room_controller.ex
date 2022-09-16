acb598d516351df4fcf8bc1a57584090c77da3bc
Angel Acuña Huerta <angel.huerta@bunsan.io>
2022-09-15 20:29:59 -0500

    create rooms

---
+++ lib/fawkes_web/controllers/room_controller.ex
@@ -0,0 +1,26 @@
+defmodule FawkesWeb.RoomController do
+  use FawkesWeb, :controller
+  alias Fawkes.Chat
+  alias FawkesWeb.Utils
+
+  def ping(conn, _params) do
+    conn
+    |> render("ack.json", %{success: true, message: "pong"})
+  end
+
+  def create(conn, params) do
+    case Chat.create_room(params) do
+      {:ok, _room} ->
+        conn
+        |> render("ack.json", %{success: true, message: "Room created"})
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        conn
+        |> render("errors.json", %{errors: Utils.format_changeset_errors(changeset)})
+
+      true ->
+        conn
+        |> render("ack.json", %{succes: false, message: Utils.internal_srever_error()})
+    end
+  end
+end
